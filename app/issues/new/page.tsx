ebd0c0de1f4bb0a2ea6446684f3165c1f8038aec
Bernd Ullmann <info@berndullmann.de>
2024-04-11 13:45:24 +0200

    handling errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React from "react";
-import { TextField, Button } from "@radix-ui/themes";
+import React, { use, useState } from "react";
+import { TextField, Button, Callout } from "@radix-ui/themes";
 import { FaBug } from "react-icons/fa";
 import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, set } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -17,30 +17,44 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
-  // console.log(register("title"));
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="Search the docs…" {...register("title")}>
-        <TextField.Slot>
-          <FaBug height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Reply to comment…" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>
+            You will need admin privileges to install and access this
+            application.
+          </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("Un unexpected error occurred. Please try again later.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Search the docs…" {...register("title")}>
+          <TextField.Slot>
+            <FaBug height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Reply to comment…" {...field} />
+          )}
+        />
 
-      <Button>Submit</Button>
-    </form>
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
